9aebfc68c8c8b72138a0486125807a01033d1e21
alisonbr33rr <anamariamachado021@gmail.com>
2026-09-01 10:33:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 
-// Rolagem suave dos links do menu
-
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-
-    link.addEventListener('click', function(e){
-
-        e.preventDefault();
-
-        const destino = document.querySelector(this.getAttribute('href'));
-
-        destino.scrollIntoView({
-            behavior:'smooth'
-        });
-
-    });
-
-});
